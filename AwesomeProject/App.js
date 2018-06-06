b0b83f819dfb743410b0c577fbfb4d1cedb3eba5
Roge-L <36053941+Roge-L@users.noreply.github.com>
2018-06-06 15:19:20 -0600

    Going through React Native Guide
    Skimming through the React Native guide

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -7,9 +7,10 @@ import Welcome from "./components/Welcome.js";
 export default class AwesomeProject extends Component {
   render() {
     return (
-      <View>
+      <View style={{flex: 1}}>
         <Welcome message="This is my message Prop given to my Welcome component"/>
         <Pug />
+        <View style={{flex:1, backgroundColor: "powderblue"}}/>
       </View>
     )
   }
--- AwesomeProject/components/Pug.js
+++ AwesomeProject/components/Pug.js
@@ -7,7 +7,7 @@ export default class Pug extends Component {
       uri: 'https://wallpapercave.com/wp/87HMOXr.jpg'
     };
     return (
-      <Image source={pug} style={{width: 412, height: 683}}/>
+      <Image source={pug} style={{width: 300, height: 400}}/>
     )
   }
 }
